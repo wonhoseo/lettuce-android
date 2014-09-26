cd25293621465ef1c6181c6ccd3f9717de971821
Wonho Seo <cforgeleader@gmail.com>
2014-09-26 20:23:30 +0900

    update adb_local_client.rb for press and touch operations

--- lib/lettuce-android/adb_local_client.rb
+++ lib/lettuce-android/adb_local_client.rb
@@ -1,8 +1,13 @@
 
 require 'lettuce-android/adb_host_client'
+require 'lettuce-android/adb_keymap'
 module Lettuce module Android
   module Operations
     class AdbLocalClient < Lettuce::Android::Operations::AdbHostClient
+      include Lettuce::Android::Operations::AdbKeymap
+      UP = 0
+      DOWN = 1
+      DOWN_AND_UP = 2
       
       #include Lettuce::Android::Operations::AdbHostClient
       VERSION_SDK_PROPRETY = 'ro.build.version.sdk'
@@ -96,7 +101,175 @@ module Lettuce module Android
         density = get_property_internal('ro.sf.lcd_density')
         return density.to_i
       end
-
+      
+      def press(name, type=DOWN_AND_UP)
+        cmd = 'input keyevent %s' % name
+        shell(cmd)
+      end
+      
+      def long_press(name, duration = 0.5, dev='/dev/input/event0')
+        if name[0..3] == 'KEY_'
+          name = name[4..-1].upcase
+        end
+        if KEY_MAP.include?(name)
+          shell('sendevent %s 1 %d 1' % [dev, KEY_MAP[name]])
+          shell('sendevent %s 0 0 0' % [dev])
+          sleep(duration)
+          shell('sendevent %s 1 %d 0' % [dev, KEY_MAP[name]])
+          shell('sendevent %s 0 0 0' % [dev])
+          return
+        end
+        version = get_sdk_version
+        if version >= 19
+          cmd = 'input keyevent --longpress %s' % name
+          shell(cmd)
+        else
+          raise RuntimeError, "long_press: not support for API < 19 (version=#{version})"
+        end
+      end
+        
+      def start_activity(component=nil, flags=nil, uri=nil)
+        cmd = 'am start'
+        cmd += ' -n %s' % component unless component.nil?
+        cmd += ' -f %s' % flags unless flags.nil?
+        cmd += ' %s' % uri unless nil
+        out = shell(cmd)
+        if %r[(Error type)|(Error: )|(Cannot find 'App')]im =~ out
+          raise RuntimeError, out
+        end
+      end
+      
+      # return image CunkyImage
+      def take_spanshot(reconnect=false)
+        raise "not yet implement"
+        
+close()
+        init_socket()
+        transport_command = 'host:transport:%s' % @serialno
+        dispatch(transport_command)            
+        dispatch('framebuffer:')
+        # case 1:// version
+        #    return 12; // bpp, size, width, height, 4*(lengh, offset)
+        received_header = receive(1*4 + 12*4)
+        # L: unsigned long(32-bit) < : little-endian *: will use up all remaining elements 
+        version, bpp, size, width, height, roffset, rlen, boffset, blen, goffset, glen, aoffset, alen \
+            = received_header.to_s.unpack('L<'*13)
+        # take_snapshot: [1, 32, 8294400, 1080, 1920, 0, 8, 16, 8, 8, 8, 24, 8]
+        logger.debug "#{TAG} take_snapshot: #{[version, bpp, size, width, height, roffset, rlen, boffset, blen, goffset, glen, aoffset, alen]}"
+        offsets = { roffset => 'R', goffset => 'G', boffset => 'B'}
+        if bpp == 32
+          if alen != 0
+            offsets[aoffset] = 'A'
+          else
+            #warnings.warn 'framebuffer is specified as 32bpp but alpha length is 0'
+            logger.warn 'framebuffer is specified as 32bpp but alpha length is 0'
+          end
+        end
+        arg_mode = ''
+        offsets.sort.each do |key,value|
+          arg_mode << value
+        end
+        logger.debug "#{TAG} take_snapshot: #{[version, bpp, size, width, height, roffset, rlen, boffset, blen, goffset, glen, aoffset, alen, arg_mode]}"
+        if arg_mode == 'BGRA'
+          arg_mode = 'RGBA'
+        end
+        if bpp == 16
+          mode = 'RGB'
+          arg_mode += ';16'
+        else
+          mode = arg_mode
+        end
+        dispatch('\0', false)
+        logger.debug "#{TAG}    takeSnapshot: reading %d bytes" % size
+        received_data = receive(size)
+        if reconnect
+          init_socket()
+          set_transport()
+        end
+        logger.debug "#{TAG}    takeSnapshot: Image.frombuffer(%s, [%d, %d], %s, %s, %s, %s, %s)" % [mode, width, height, 'data', 'raw', arg_mode, 0, 1]
+        
+        # StreamImporting
+        # #from_abgr_stream, #from_bgr_stream, #from_rgb_stream, #from_rgba_stream
+        if arg_mode == 'RGBA'        
+          image = ChunkyPNG::Image.from_rgba_stream(width, height, received_data)
+        elsif arg_mode == 'ABGR'
+          image = ChunkyPNG::Image.from_abgr_stream(width, height, received_data) 
+        elsif arg_mode == 'RGB'
+          image = ChunkyPNG::Image.from_rgb_stream(width, height, received_data) 
+        elsif arg_mode == 'BGR'
+          image = ChunkyPNG::Image.from_bgr_stream(width, height, received_data) # 1 sec
+        else
+          logger.warn "#{TAG}   take_snapshot unsupport mode %s" % arg_mode     
+        end
+           
+        logger.debug "#{TAG} save : my_file.png"
+        image.save('my_file.png') # 6 secs
+        logger.debug "#{TAG} save : done"        
+      end
+      
+      def touch(x,y,event_type=DOWN_AND_UP)
+        cmd = "input tap %d %d" % [x, y]
+        shell(cmd)        
+      end
+      
+      def drag((x0,y0), (x1,y1), duration, step=1)
+        logger.debug("#{TAG} drag([#{x0}, #{y0}], [#{x1}, #{y1}],#{duration},#{step})")
+        version = get_sdk_version
+        if version <= 15
+          raise RuntimeError, "drag: API <= 15 not supported (version=%d)" % version
+        elsif version <= 17
+          shell('input swipe %d %d %d %d' % [x0, y0, x1, y1])
+        else
+          shell('input touchscreen swipe %d %d %d %d %d' % [x0, y0, x1, y1, duration])
+        end
+      end
+      
+      def wake
+        unless is_screen_on
+          shell('input keyevent POWER')
+        end
+      end
+      
+      def is_screen_on
+        logger.debug "#{TAG} is_screen_on()"
+        result = false
+        screen_on_re = /mScreenOnFully=(true|false)/
+        recv = shell('dumpsys window policy')
+        recv.each_line do |line|
+          logger.debug line
+          if m = screen_on_re.match(line)
+            logger.debug m
+            result = (m[1] == 'true')
+            break
+          end
+        end
+        logger.debug "#{TAG}    is_screen_on return #{result}"
+        return result
+      end
+      
+      def is_locked
+        logger.debug "#{TAG} is_locked()"
+        result = false
+        showing_lock_screen_re = /mShowingLockscreen=(true|false)/
+        recv = shell('dumpsys window policy')
+        recv.each_line do |line|
+          logger.debug line
+          if m = showing_lock_screen_re.match(line)
+            logger.debug m
+            result = (m[1] == 'true')
+            break
+          end
+        end
+        logger.debug "#{TAG}    is_locked return #{result}"
+        return result        
+      end
+      
+      def unlock
+        # Unlocks the screen of the device.
+        shell('input keyevent MENU')
+        shell('input keyevent BACK')
+        # TODO wait and check is_locked value
+      end
     end
   end
 end end
--- lib/lettuce-android/device.rb
+++ lib/lettuce-android/device.rb
@@ -8,9 +8,28 @@ module Lettuce module Android module Operations
 
     # @return [Device] a new Device instance 
     def initialize options={}
+      init_logger()
       super options
     end
     
+    def logger
+      @logger_
+    end
+
+    private
+    def init_logger
+      log = Logger.new(STDOUT)
+      log.progname = "Device"
+      #log.datetime_format = "%Y-%m-%d %H:%M:%S.%L"
+      log.datetime_format = "%m-%d %H:%M:%S.%6N"
+      #log.formatter = proc do |severity, datetime, progname, msg|
+      #  "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")},#{severity}, #{progname} - #{msg}\n"
+      #end        
+      # FATAL ERROR WARN INFO DEBUG
+      #log.level = Logger::WARN
+      log.level = Logger::DEBUG
+      @logger_ = log 
+    end
   end
   
 end end end
